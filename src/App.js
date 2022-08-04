7d63ec5daa42adb6fca8b7760911446b30065264
Mohamed Bakr <moh.bakr27@gmail.com>
2022-08-04 18:36:14 +0200

    GoogleAuth

--- src/App.js
+++ src/App.js
@@ -2,41 +2,50 @@ import React from 'react';
 import styled from 'styled-components';
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route
+    BrowserRouter as Router,
+    Switch,
+    Route
 } from "react-router-dom";
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
+import {useAuthState} from "react-firebase-hooks/auth";
+import {auth} from "./firebase";
+import Login from "./components/Login";
 
 function App() {
-  return (
-    <div className="app">
-      <Router>
-        <>
-          <Header />
-          <AppBody>
-            <Sidebar />
-            <Switch>
-              <Route path="/">
-                <Chat />
+    const [user, loading] = useAuthState(auth);
+    return (
+        <div className="app">
+            <Router>
+                {!user ? (
+                    <Login/>
+                ) : (
+                    <>
+                        <Header/>
+                        <AppBody>
+                            <Sidebar/>
+                            <Switch>
+                                <Route path="/">
+                                    <Chat/>
 
-              </Route>
-            </Switch>
-          </AppBody>
+                                </Route>
+                            </Switch>
+                        </AppBody>
 
-        </>
-      </Router>
+                    </>
+                )}
 
-    </div>
+            </Router>
 
-  );
+        </div>
+
+    );
 }
 
 export default App;
 
 const AppBody = styled.div`
-display: flex;
-height: 100vh;
+  display: flex;
+  height: 100vh;
 `;
--- src/components/Chat.js
+++ src/components/Chat.js
@@ -72,7 +72,7 @@ function Chat() {
                         channelId={roomId}
 
                     />
-                </> 
+                </>
             )}
 
 
